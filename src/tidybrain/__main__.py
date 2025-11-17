cec0070700e92711014eb6ba765b19febedb61de
martin gaborit <mgaborit.dev@gmail.com>
2025-11-17 20:53:20 +0100

    feat (__main__.py): assigning workspace with TB_HOME env var

--- src/tidybrain/__main__.py
+++ src/tidybrain/__main__.py
@@ -1,8 +1,19 @@
 """Entry point for Tidy Brain application."""
+import os
+import sys
+
 from .brain import Brain
 from .console import Interpreter
 
-CONFIG_FILE = "/home/martin/dev/sandbox/tidybrain/brain.json"
+HOME_DIR = os.environ.get("TB_HOME")
+if HOME_DIR is None:
+    print("Tidy Brain home directory is not set. Please set the TB_HOME environment variable.")
+    sys.exit(1)
+
+if not os.path.exists(HOME_DIR):
+    os.makedirs(HOME_DIR)
+
+CONFIG_FILE = os.path.join(HOME_DIR, "brain.json")
 
 brain = Brain()
 brain.load(CONFIG_FILE)
